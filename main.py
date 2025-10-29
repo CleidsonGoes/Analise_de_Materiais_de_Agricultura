a19291b1a1d997f1b4dac5f47b6247a5419386bc
Cleidson Goes <jorgegoes4@gmail.com>
2025-10-28 22:03:11 -0300

    Atualização da extract e da main.py para registro de logs

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ Módulo principal da pipeline de Análise de Dados de Materiais de agricultura
 Este script é o ponto de entrada do projeto e executa a primeira etapa da
 pipeline ETL: a **extração** dos dados brutos armazenados na pasta `data/raw/`.
 
+Etapas:
+1. Configura o sistema de logs.
+2. Chama a função de extração dos dados brutos (Extract).
+3. Exibe no terminal o resultado da execução.
+
 Etapas futuras:
 - Transformação: limpeza e padronização dos dados.
 - Carga: exportação dos dados tratados para a pasta `data/processed/` ou para
@@ -18,16 +23,36 @@ Autor: Cleidson Goes
 Data: 2025-10-27
 """
 
+import logging
 from src.etl.extract import extrair_dados
 
 
+# Configuração de logging
+def configurar_logs():
+    """Configura o sistema de logging para console e arquivo."""
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s - %(levelname)s - %(message)s",
+                        handlers=[
+                            logging.FileHandler("logs/etl_pipelin.log",
+                                                mode="a", encoding="utf-8"),
+                            logging.StreamHandler()  # exibe no terminal também
+                            ]
+                        )
+
+
 def main():
-    """Função principal que orquestra a execução da etapa de extração."""
+    """Executa a pipeline de ETL (por enquanto, apenas a etapa Extract)."""
 
-    caminho = "data/raw/agricultural_raw_material.csv"
-    df = extrair_dados(caminho)
+    configurar_logs()
+    caminho_arquivo = "data/raw/agricultural_raw_material.csv"
 
-    print(df.head())
+    logging.info("Iniciando pipeline de ETL...")
+
+    try:
+        df = extrair_dados(caminho_arquivo)
+        logging.info("Extração concluída. Total de linhas: %s", len(df))
+    except Exception as e:  # noqa: W0718
+        logging.error("Falha inesperada na pipeline: %s", e)
 
 
 if __name__ == "__main__":
--- src/etl/extract.py
+++ src/etl/extract.py
@@ -16,7 +16,7 @@ dados = extrair_dados("data/raw/agricultural_raw_material.csv")
 print(dados.head())
 """
 
-from pathlib import Path
+import logging
 import pandas as pd
 
 
@@ -28,18 +28,29 @@ def extrair_dados(caminho_arquivo: str) -> pd.DataFrame:
         caminho_arquivo (str): Caminho completo do arquivo CSV.
 
     Retorna:
-        pd.Dataframe: Dados extraidos.
+        pd.Dataframe: Dados carregados em formato tabular.
+
+    Raises:
+        FileNotFoundError: Se o arquivo não for encontrado.
+        pd.errors.EmptyDataError: Se o arquivo estiver vazio.
+        pd.errors.ParserError: Se o arquivo estiver corrompido ou mal formatado
     """
 
-    caminho = Path(caminho_arquivo)
+    logging.info("Iniciando extração de dados do arquivo: %s", caminho_arquivo)
 
-    if not caminho.exists():
-        raise FileNotFoundError(f"Arquivo não encontrado: {caminho.resolve()}")
+    try:
+        df = pd.read_csv(caminho_arquivo)
+        logging.info("Extração sucedida. Total de linhas: %s", len(df))
+        return df
 
-    print(f"Extraindo dados de: {caminho.resolve()}")
+    except FileNotFoundError as e:
+        logging.error("Arquivo não encontrado: %s", caminho_arquivo)
+        raise e
 
-    df = pd.read_csv(caminho)
+    except pd.errors.EmptyDataError as e:
+        logging.error("O arquivo %s está vazio.", caminho_arquivo)
+        raise e
 
-    print(f"Extração sucedida! {df.shape[0]} linhas e {df.shape[1]} colunas.")
-
-    return df
+    except pd.errors.ParserError as e:
+        logging.error("Erro ao analisar o arquivo CSV: %s", caminho_arquivo)
+        raise e
